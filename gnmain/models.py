12191833d44d5bd6503cf33411a8b5b80b3ffc63
Marcel Gaedke <marcelgaedke@yahoo.com>
2018-09-27 17:56:34 +0200

    created Category, Picture classes

--- gnmain/models.py
+++ gnmain/models.py
@@ -3,3 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Picture(models.Model):
+    #category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    picture_title = models.CharField(max_length=200)
+    #votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.picture_title
+
+
