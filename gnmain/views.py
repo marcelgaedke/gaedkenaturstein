f0f9de431847481d5f3484dc94286e92486b7d8a
Marcel Gaedke <marcelgaedke@yahoo.com>
2018-09-27 19:05:42 +0200

    created index view

--- gnmain/views.py
+++ gnmain/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+'''from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("GN Main")
 
+'''
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Category, Picture
+
+
+class IndexView(generic.ListView):
+    model=Category
+    template_name = 'gnmain/index.html'
+    #context_object_name = 'latest_question_list'
