6997a426e47d275e88dd842ca75caf0986d257f9
Marcel Gaedke <marcelgaedke@yahoo.com>
2018-09-28 18:59:46 +0200

    created header css

--- gaedkenaturstein/settings.py
+++ gaedkenaturstein/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'django.contrib.staticfiles',
     'gnmain.apps.GnmainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
