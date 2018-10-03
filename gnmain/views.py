be2225cdafd8540acbafcc2c291198f89bfed778
Marcel Gaedke <marcelgaedke@yahoo.com>
2018-10-03 19:22:16 +0200

    created Picture Gallery

--- gnmain/views.py
+++ gnmain/views.py
@@ -34,9 +34,21 @@ class DetailView(generic.DetailView):
 
 
     def get_context_data(self, **kwargs):
-        #img_list = os.listdir("/var/www/ssd1400/htdocs/App1/static/App/pictures/Header")
-        img_list = os.listdir("/home/kidneybean/PycharmProjects/gaedkenaturstein/gnmain/static/gnmain/pictures/Header")
         context = super(DetailView, self).get_context_data(**kwargs)
+        cat = context['category']
+        #path1 = "/var/www/ssd1400/htdocs/App1/static/"
+        path1 = "/home/kidneybean/PycharmProjects/gaedkenaturstein/gnmain/static/"
+        #path2 = "App/pictures/"+ cat.category_name +"/"
+        path2 = "gnmain/pictures/"+ cat.category_name +"/"
+        path = path1 + path2
+        filelist = os.listdir(path)
+        img_list=[]
+        for f in filelist:
+            if "." in f:
+                i = f.index(".")
+                file ={'filename':(path2+f),'description':f[:i]}
+                img_list.append(file)
+
         context['category_list'] = Category.objects.all()
         context['img_list'] = img_list
         return context
